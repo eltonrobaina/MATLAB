20c698df9e5d89fcb48c12bbcfe9993d431d7e70
Elton John Robaina da Silva <68453073+eltonrobaina@users.noreply.github.com>
2021-02-21 10:14:49 -0300

    exercícios da apostila

---
+++ Revisão/Aula01.m
@@ -0,0 +1,36 @@
+a = 2^5/(2^5-1);
+b = (1-1/2^5)^-1;
+c = 3*(sqrt(5)-1)/(sqrt(5)+1)^2-1;
+r = pi^(1/3)-1;
+d = pi*r^2;
+e = exp(3);
+f = log(exp(3));
+g = log10(exp(3));
+h = exp(pi*sqrt(163));
+I = sin(pi/6);
+J = (sin(pi/6))^2;
+k = atan(sqrt(3)/3);
+l = atan(tan(pi/6));
+m = atan(tan(5*pi/6));
+n = sqrt(-1);
+z = (1+3i)/(1-3i);
+o = abs(z);
+theta = angle(z);
+p = convert2deg(theta);
+q = real(z);
+s = imag(z);
+
+A = floor(-pi);
+B = ceil(-pi);
+C = fix(3);
+D = fix(-3);
+E = mod(pi,2);
+F = mod(26,2);
+G = mod(-26,5);
+H = rem(26,5);
+L = sign(-4);% retorna -1, pois o valor da função é < 0.
+M = sign(pi);%retorna 1, pois o valor da função é >0!
+
+
+
+
